62261b25e2a0f4207796ef4997662eec9621262d
Rodolfo P A <6721075+rodoufu@users.noreply.github.com>
2023-12-27 11:27:47 -0300

    Update args.rs

--- src/args.rs
+++ src/args.rs
@@ -1,11 +1,11 @@
 use clap::Parser;
 use std::sync::Arc;
 
-/// Checks the dependencies of the subprojects of a Rust workspace.
+/// Checks the dependencies of member projects of a Rust workspace looking for shared ones that could be added to the workspace dependencies.
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 pub(crate) struct Args {
-    /// Path to the project to get the workspace and check the dependencies
+    /// Path to the workspace it should evaluate the member projects and check the dependencies
     #[arg(short, long)]
     pub(crate) project_path: Arc<str>,
 }
